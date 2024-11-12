51e3fd011453b14e72573c76de64b3b897a1fce3
Dylan Balagtas <dylansatgalab@gmail.com>
2024-11-12 14:05:00 +0800

    Initial Commit

---
+++ medicinetracker/medicinetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  medicinetrackerApp.swift
+//  medicinetracker
+//
+//  Created by Dylan Balagtas on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct medicinetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
